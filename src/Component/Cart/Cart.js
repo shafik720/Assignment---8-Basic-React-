e8cfc152c403402585031133e40ee057e8646d17
shafik720 <shafikrasel5@gmail.com>
2022-10-29 00:20:16 +0600

    sdfgfsdg

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -3,10 +3,14 @@ import './Cart.css'
 
 const Cart = (props) => {
     let cart = props.cart;
+    let quantity = 0;
+    for(let element of cart){
+        quantity += element.quantity;
+    }
     return (
         <div className="cart-div">
             <h2>Order summary : </h2>
-            <p>Total Added : {cart.length} </p>
+            <p>Total Added : {quantity} </p>
         </div>
     );
 };
