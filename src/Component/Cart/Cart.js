b172d06d9f5946d0c0899dad9c6549b734afddee
shafik720 <shafikrasel5@gmail.com>
2022-10-29 00:31:04 +0600

    shipping charge added

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -4,13 +4,26 @@ import './Cart.css'
 const Cart = (props) => {
     let cart = props.cart;
     let quantity = 0;
+    let totalPrice = 0;
+    let shippingFee = 0;
+    let tax = 0;
+    let grandTotal = 0;
     for(let element of cart){
         quantity += element.quantity;
+        totalPrice += (element.price * element.quantity);   
+        shippingFee += (element.quantity * 100);
     }
+    if(totalPrice>8000){
+        shippingFee = 0 ;
+    }
+    tax = (totalPrice * 0.1);
     return (
         <div className="cart-div">
             <h2>Order summary : </h2>
             <p>Total Added : {quantity} </p>
+            <p>Total Price : {totalPrice} </p>
+            <p>Tax : {tax}</p>
+            <p>Shipping Charge : {shippingFee}</p>
         </div>
     );
 };
