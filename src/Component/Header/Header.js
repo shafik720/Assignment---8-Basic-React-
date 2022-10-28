246903e629a28405a8c4ded1253e7dd95713c29c
shafik720 <shafikrasel5@gmail.com>
2022-10-28 20:28:40 +0600

    sdfgfsdg

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -6,17 +6,19 @@ import { faUserTie } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
     return (
-        <div className="header-div">
-            <div className="left-side">
-                <span><FontAwesomeIcon icon={faUserTie} /></span>
-                <h2>Gentelmen's Cart</h2>
-            </div>
-            <div className="right-side">
-                <a href="/home">Home</a>
-                <a href="/home">About</a>
-                <a href="/home">Contact</a>
-                <a href="/home">Menu</a>
-                <a href="/home">Shop</a>
+        <div className="header-divs">
+            <div className="header-div">
+                <div className="left-side">
+                    <span><FontAwesomeIcon icon={faUserTie} /></span>
+                    <h2>Gentelmen's Cart</h2>
+                </div>
+                <div className="right-side">
+                    <a href="/home">Home</a>
+                    <a href="/home">About</a>
+                    <a href="/home">Contact</a>
+                    <a href="/home">Menu</a>
+                    <a href="/home">Shop</a>
+                </div>
             </div>
         </div>
     );
