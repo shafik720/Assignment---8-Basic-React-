866b387751a0578ec9d43490b4e7a612115b9bba
shafik720 <shafikrasel5@gmail.com>
2022-10-29 00:42:37 +0600

    shipping charge added

--- src/Component/CartCad/CartCard.js
+++ src/Component/CartCad/CartCard.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './CartCard.css'
 
 const CartCard = (props) => {
+    const{name, price, quantity, img} = props.index;
     return (
         <div className="cart-card">
             <div className="cart-left">
-                <div className="cart-img"></div>
-                <div className="cart-details"></div>
+                <div className="cart-img">
+                    <img src={img} alt=""/>
+                </div>
+                <div className="cart-details">
+                    <p>{price}</p>
+                    <p>{quantity}</p>
+                </div>
             </div>
             <div className="cart-right"></div>
         </div>
