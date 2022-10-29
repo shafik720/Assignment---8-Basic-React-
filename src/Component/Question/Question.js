eb305653d9614ed20a2bba9ab2d7d4d54ebce486
shafik720 <shafikrasel5@gmail.com>
2022-10-29 18:47:47 +0600

    sdfgsg

--- src/Component/Question/Question.js
+++ src/Component/Question/Question.js
@@ -10,9 +10,11 @@ const Question = () => {
                 <p>React goes through its virtual DOM ,creates a list of those changes that need to be made to the actual DOM and then does it all in one single process.  React will have trees of objects on the virtual DOM and trees of objects on the real DOM. React automatically updates the associated DOM element when we change data on a React element.</p>
             </div>
             <div className="answer-div">
-                <h2>How React Works ? </h2>
+                <h2>How React Usestate Works ? </h2>
                 <hr/>
-                <p>React goes through its virtual DOM ,creates a list of those changes that need to be made to the actual DOM and then does it all in one single process.  React will have trees of objects on the virtual DOM and trees of objects on the real DOM. React automatically updates the associated DOM element when we change data on a React element.</p>
+                <p>The useState hook allows us declare one or more state variables in function components. Under the hood, React keeps track of these state variables and ensures they stay up-to-date on subsequent re-renders of the component. 
+                It's a Hook that allows to have state variables in functional components. You pass the initial state to this function and it returns a variable with the current state value (not necessarily the initial state) and another function to update this value.
+                </p>
             </div>
         </div>
     );
